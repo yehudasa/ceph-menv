0517dda54868bb21ab15fa549680aed662471cbd
Yehuda Sadeh <yehuda@redhat.com>
2018-05-21 13:33:02 -0700

    build_links: can run from different dir, create bin directory
    Signed-off-by: Yehuda Sadeh <yehuda@redhat.com>

--- build_links.sh
+++ build_links.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+DIR=`dirname $0`
 ROOT=$1
 
 [ "$ROOT" == "" ] && ROOT="$HOME/ceph"
 
+mkdir -p $DIR/bin
+
 echo $PWD
 for f in `ls $ROOT/build/bin`; do
         echo $f
-        ln -sf ../mdo.sh bin/$f
+        ln -sf ../mdo.sh $DIR/bin/$f
 done
 
-DIR=`dirname $0`
 echo "MRUN_CEPH_ROOT=$ROOT" > $DIR/.menvroot
