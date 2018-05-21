06c13dc64f912d57ff8d48a69f369a2503b1860c
Yehuda Sadeh <yehuda@redhat.com>
2018-05-21 13:26:21 -0700

    menv: simplify installation, configurable location
    Signed-off-by: Yehuda Sadeh <yehuda@redhat.com>

--- build_links.sh
+++ build_links.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
+
+ROOT=$1
+
+[ "$ROOT" == "" ] && ROOT="$HOME/ceph"
+
 echo $PWD
-for f in `ls $HOME/ceph/build/bin`; do
+for f in `ls $ROOT/build/bin`; do
         echo $f
         ln -sf ../mdo.sh bin/$f
 done
+
+DIR=`dirname $0`
+echo "MRUN_CEPH_ROOT=$ROOT" > $DIR/.menvroot
--- mdo.sh
+++ mdo.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 cmd=`basename $0`
+MENV_ROOT=`dirname $0`/..
+
+if [ -f $MENV_ROOT/.menvroot ]; then
+    . $MENV_ROOT/.menvroot
+fi
+
+[ "$MRUN_CEPH_ROOT" == "" ] && MRUN_CEPH_ROOT=$HOME/ceph
+
 if [ "$MRUN_CLUSTER" == "" ]; then
-    ~/ceph/build/bin/$cmd "$@"
+    ${MRUN_CEPH_ROOT}/build/bin/$cmd "$@"
     exit $?
 fi
 
-mrun $MRUN_CLUSTER $cmd "$@"
+${MRUN_CEPH_ROOT}/src/mrun $MRUN_CLUSTER $cmd "$@"
